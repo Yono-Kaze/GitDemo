eb5264f24e32453fcb34b3fdb877271c6c4c56b6
YonoKaze <450076356@qq.com>
2020-11-06 18:28:36 +0800

    数据库sql文件更新

--- sql/manger_dept.sql
+++ sql/manger_dept.sql
@@ -28,6 +28,16 @@ CREATE TABLE `dept` (
   PRIMARY KEY (`deptid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dept`
+--
+
+LOCK TABLES `dept` WRITE;
+/*!40000 ALTER TABLE `dept` DISABLE KEYS */;
+INSERT INTO `dept` VALUES (1,'测试部'),(2,'发售部');
+/*!40000 ALTER TABLE `dept` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -38,4 +48,4 @@ CREATE TABLE `dept` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-16 20:00:05
+-- Dump completed on 2020-11-06 18:27:53
